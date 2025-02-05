452c31075b9a41b67af527358cec8917699b9f67
CPTMILTOON <cristobalvivanco78@gmail.com>
2025-02-05 17:08:56 -0300

    Actualizada configuracion de Stackbit

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,14 +2,14 @@ import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
   stackbitVersion: '0.5.3',
-  ssgName: 'hugo',
-  nodeVersion: '18',
-  devCommand: 'hugo server',
+  ssgName: 'hugo',  // Especifica que estás usando Hugo
+  nodeVersion: '18',  // Usa la versión de Node.js que necesites
+  devCommand: 'hugo server',  // Comando para iniciar el servidor de desarrollo
   contentSources: [
     {
       type: 'git',
-      rootPath: '.',
-      contentDir: 'content',
+      rootPath: '.',  // Ruta raíz del proyecto
+      contentDir: 'content',  // Carpeta donde está el contenido
     },
   ],
 });
